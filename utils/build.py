302a3b309c41cd7c959e94aef5505f8560abe5e6
Pyae <pyaesonenyohmine143@gmail.com>
2021-07-28 15:16:14 -0400

    differentiate between slanted and rotated AR tag for orientation

--- utils/build.py
+++ utils/build.py
@@ -10,6 +10,8 @@ font_size = 0.7
 color = (255, 0, 0)
 thickness = 2
 
+"""FUNCTIONS FOR CONTROLLING DRONE MANUALLY AND WITH AR MARKER"""
+
 
 # detects the ar marker and gives values of 4 corners and ids
 def ar_detector(img, draw_box_id=True, draw_px_val=False):
@@ -165,9 +167,12 @@ def follow_ar_with_PID(my_ar, kp, ki, kd, prev_d_error):
 def slope_orientation_with_PID(my_ar, kp, ki, kd, prev_s_error):
     my_ar.orientation_func()
 
-    s_error = my_ar.slope
+    s_error = my_ar.slope_x
     s_speed = kp * s_error + ki * s_error + kd * (s_error - prev_s_error)
     s_speed = int(np.clip(s_speed, -100, 100))
+
+    if abs(my_ar.slope_y) > 0.1:
+        s_speed = 0
 
     return s_error, - s_speed
 
@@ -182,3 +187,97 @@ def traffic_action_with_ar(my_ar, my_drone, ids):
             my_drone.move_up(30)  # move up
         elif ids[0][0] == 3:
             my_drone.move_down(30)  # move down
+
+
+""" FUNCTIONS FOR COLOR DETECTION"""
+
+
+def get_colors(img):
+    kernel = np.ones((7, 7), np.uint8)
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    lower_green = np.array([40, 35, 90])  # adjust as needed
+    upper_green = np.array([85, 255, 255])  # adjust as needed
+    mask_green = cv2.inRange(hsv, lower_green, upper_green)
+    res_green = cv2.bitwise_and(img, img, mask=mask_green)
+    opening_green = cv2.morphologyEx(mask_green, cv2.MORPH_OPEN, kernel)
+    contours_green, _ = cv2.findContours(opening_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    lower_yellow = np.array([20, 35, 90])  # adjust as needed
+    upper_yellow = np.array([26, 255, 255])  # adjust as needed
+    mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    res_yellow = cv2.bitwise_and(img, img, mask=mask_yellow)
+    opening_yellow = cv2.morphologyEx(mask_yellow, cv2.MORPH_OPEN, kernel)
+    contours_yellow, _ = cv2.findContours(opening_yellow, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    lower_red1 = np.array([0, 25, 90])  # adjust as needed
+    upper_red1 = np.array([8, 255, 255])  # adjust as needed
+    lower_red2 = np.array([173, 25, 90])  # adjust as needed
+    upper_red2 = np.array([180, 255, 255])  # adjust as needed
+    mask1_red = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2_red = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask_red = cv2.bitwise_or(mask1_red, mask2_red)
+    res_red = cv2.bitwise_and(img, img, mask=mask_red)
+    opening_red = cv2.morphologyEx(mask_red, cv2.MORPH_OPEN, kernel)
+    contours_red, _ = cv2.findContours(opening_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    return contours_green, contours_yellow, contours_red
+
+
+def find_size(contours):
+    if len(contours) == 0:
+        return 0
+
+    con_x = list([i[0] for i in contours])[0][0]
+    con_y = list([i[1] for i in contours])[0][0]
+    #print(con_x)
+    #print(con_y)
+
+    max_x = con_x[0]
+    min_x = con_x[0]
+    #print(max_x)
+    #print(min_x)
+
+    max_y = con_y[0]
+    min_y = con_y[0]
+    #print(max_y)
+    #print(min_y)
+
+    for i in con_x:
+        if i > max_x:
+            max_x = i
+        if i < min_x:
+            min_x = i
+
+    for i in con_y:
+        if i > max_y:
+            max_y = i
+        if i < min_y:
+            min_y = i
+
+    xlen = max_x - min_x
+    ylen = max_y - min_y
+
+    return xlen * ylen
+
+
+def find_largest(green_cont, yellow_cont, red_cont):
+    green_area = find_size(green_cont)
+    yellow_area = find_size(yellow_cont)
+    red_area = find_size(red_cont)
+
+    if (green_area >= yellow_area) and (green_area >= red_area):
+        largest = green_area
+
+    elif (yellow_area >= green_area) and (yellow_area >= red_area):
+        largest = yellow_area
+    else:
+        largest = red_area
+
+    if largest == green_area:
+        return 0
+    elif largest == yellow_area:
+        return 1
+    elif largest == red_area:
+        return 2
